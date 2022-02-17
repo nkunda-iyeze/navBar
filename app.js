4f34996c7e4671a41dc7ffb6b2af4ee1ee8c707c
parlom <shalom.iyeze@gmail.com>
2022-02-17 15:35:31 +0200

    navBar project completed well

--- app.js
+++ app.js
@@ -3,3 +3,21 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(`.nav-toggle`);
+
+const linksList = document.querySelector(`.links`);
+
+navToggle.addEventListener(`click`, function () {
+    // console.log(linksList.classList.contains(`show-links`))
+    // console.log(linksList.classList.contains(`links`))
+
+    // longest way to achieve the movements of this array
+    // let classStorage = linksList.classList;
+    // if (classStorage.contains(`show-links`)) {
+    //     classStorage.remove(`show-links`);
+    // } else {
+    //     classStorage.add(`show-links`);
+    // }
+    linksList.classList.toggle('show-links');
+})
